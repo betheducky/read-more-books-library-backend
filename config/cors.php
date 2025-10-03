b15ce017fca0da91cba3a026a8df3f36c00292fc
Aren Bruce <devtodaydevtomorrow@gmail.com>
2025-10-02 22:38:04 -0400

    update to CORS policy allowing multiple origins

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://arenbruce.com'],
+    'allowed_origins' => ['http://arenbruce.com', 'http://localhost:4200', 'http://arenbruce.com/book-search-app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
